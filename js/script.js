2d526812001622ca164d9e41523e723baaea5afe
Jesus Aguilar <enrique.prado207@gmail.com>
2023-04-30 11:07:25 -0600

    Add Swiper slides: Css and JavaScript logic

--- js/script.js
+++ js/script.js
@@ -7,3 +7,20 @@ document.querySelector('#menu-btn').onclick = () =>{
 window.onscroll = () =>{
    navbar.classList.remove('active');
 }
+
+var swiper = new Swiper(".home-slider", {
+   loop: true,
+   effect: "coverflow",
+   grabCursor: true,
+   coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: false,
+    },
+   navigation: {
+     nextEl: ".swiper-button-next",
+     prevEl: ".swiper-button-prev",
+   },
+ });
