424b3c35e1f3e7e3f62aa51bb3bd826a1baf6f8d
Jesus Aguilar <enrique.prado207@gmail.com>
2023-05-05 11:18:47 -0600

    Add Contact section

--- js/script.js
+++ js/script.js
@@ -8,6 +8,12 @@ window.onscroll = () =>{
    navbar.classList.remove('active');
 }
 
+document.querySelectorAll('.contact .row .faq .box h3').forEach(faqBox => {
+   faqBox.onclick = () =>{
+      faqBox.parentElement.classList.toggle('active');
+   }
+});
+
 var swiper = new Swiper(".home-slider", {
    loop: true,
    effect: "coverflow",
